f4b5216272c814024abe324e23ac81a0af73b70c
Veronica Marini <veronica@pspdfkit.com>
2025-01-16 11:58:58 +0100

    ignore some folders

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import pluginJs from "@eslint/js";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: ["**/assets/**"],
+    ignores: ["**/assets/**", "**/node_modules/**", "**/public/**"],
   },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
