42f4481183df6dcbc9cebdf326b5e1166a3bd813
Narashiman <156251607+narashiman-krishnamurthy@users.noreply.github.com>
2025-03-03 17:16:45 +0000

    Update vite.config.js

--- web/signing/clientside-digital-sigature/vite.config.js
+++ web/signing/clientside-digital-sigature/vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/sign': {
-        target: 'http://localhost:6000',
+      "/sign": {
+        target: "http://localhost:6000",
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
+        secure: false,
+      },
+    },
+  },
+});
