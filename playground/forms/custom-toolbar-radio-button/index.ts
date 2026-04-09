bb6208b47955697627bf2fa78232fc3d0f74a3b6
Omar <omar.talaat@pspdfkit.com>
2026-04-09 19:10:37 +0200

    applying feedback
    - corrected the example's code and link
    - added `NutrientViewer`

--- playground/forms/custom-toolbar-radio-button/index.ts
+++ playground/forms/custom-toolbar-radio-button/index.ts
@@ -4,11 +4,11 @@ import { baseOptions } from "../../shared/base-options";
 let instance: Instance | null = null;
 
 const createGroupedRadioButtons = async (instance: Instance | null) => {
-  const radioWidget1 = new window.NutrientViewer.Annotations.WidgetAnnotation({
-    id: window.NutrientViewer.generateInstantId(),
+  const radioWidget1 = new NutrientViewer.Annotations.WidgetAnnotation({
+    id: NutrientViewer.generateInstantId(),
     pageIndex: 0,
     formFieldName: "MyFormField",
-    boundingBox: new window.NutrientViewer.Geometry.Rect({
+    boundingBox: new NutrientViewer.Geometry.Rect({
       left: 100,
       top: 100,
       width: 20,
@@ -16,11 +16,11 @@ const createGroupedRadioButtons = async (instance: Instance | null) => {
     }),
   });
 
-  const radioWidget2 = new window.NutrientViewer.Annotations.WidgetAnnotation({
-    id: window.NutrientViewer.generateInstantId(),
+  const radioWidget2 = new NutrientViewer.Annotations.WidgetAnnotation({
+    id: NutrientViewer.generateInstantId(),
     pageIndex: 0,
     formFieldName: "MyFormField",
-    boundingBox: new window.NutrientViewer.Geometry.Rect({
+    boundingBox: new NutrientViewer.Geometry.Rect({
       left: 130,
       top: 100,
       width: 20,
@@ -28,18 +28,18 @@ const createGroupedRadioButtons = async (instance: Instance | null) => {
     }),
   });
 
-  const formField = new window.NutrientViewer.FormFields.RadioButtonFormField({
+  const formField = new NutrientViewer.FormFields.RadioButtonFormField({
     name: "MyFormField",
-    annotationIds: window.NutrientViewer.Immutable.List([
+    annotationIds: new NutrientViewer.Immutable.List([
       radioWidget1.id,
       radioWidget2.id,
     ]),
-    options: window.NutrientViewer.Immutable.List([
-      new window.NutrientViewer.FormOption({
+    options: new NutrientViewer.Immutable.List([
+      new NutrientViewer.FormOption({
         label: "Option 1",
         value: "1",
       }),
-      new window.NutrientViewer.FormOption({
+      new NutrientViewer.FormOption({
         label: "Option 2",
         value: "2",
       }),
@@ -57,11 +57,11 @@ const item: ToolbarItem = {
   onPress: () => createGroupedRadioButtons(instance),
 };
 
-window.NutrientViewer.load({
+NutrientViewer.load({
   ...baseOptions,
-  theme: window.NutrientViewer.Theme.DARK,
+  theme: NutrientViewer.Theme.DARK,
   toolbarItems: [
-    ...window.NutrientViewer.defaultToolbarItems,
+    ...NutrientViewer.defaultToolbarItems,
     { type: "form-creator" },
   ],
 }).then((_instance: Instance) => {
